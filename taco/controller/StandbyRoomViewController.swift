9aaea795fce6ce5fb0f2c8f9aa9424349091993a
Franky <1731012.franky@uib.edu>
2020-11-12 12:46:54 +0700

    Revise standby

--- taco/controller/StandbyRoomViewController.swift
+++ taco/controller/StandbyRoomViewController.swift
@@ -10,21 +10,86 @@ import UIKit
 
 class StandbyRoomViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var roomCode: UILabel!
+    @IBOutlet weak var statusMetting : UILabel!
     
+    var finalRoomCode:String = ""
+    let defaults = UserDefaults.standard
+    var timer = Timer()
+       
+      
+       override func viewDidLoad() {
+           super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+            roomCode.text = "Room Code \(room_code())"
+            scheduledTimerWithTimeInterval()
+        // Tadi salah dsini
+       }
+    
+        func scheduledTimerWithTimeInterval() {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(checkStatusAPI), userInfo: nil, repeats: true)
+        }
+       
+    @objc func checkStatusAPI(){
+       guard let gitURL = URL(string: "http://api.likeholidaybatam.com/check_status_meeting.php") else {return}
+       
+       var request = URLRequest(url: gitURL)
+           request.httpMethod = "POST"
+           
+       
+           let stringPost="room_code=\(room_code())" // Key and Value 
+        
+            print(stringPost)
+           let data = stringPost.data(using: .utf8)
+               request.httpBody=data
+             
+       URLSession.shared.dataTask(with: request) { (data, respone, error) in
+           
+       guard let data = data else { return }
+           do {
+                let decoder = JSONDecoder()
+                let roomData = try decoder.decode(CheckStatus.self, from: data)
+           
+            print(roomData.status!)
+            DispatchQueue.main.async {
+                
+                if roomData.status! == false{
+                    self.statusMetting.text="Wait for the Host to start the meeting"
+                }else{
+                    self.statusMetting.text="Ready"
+                    self.performSegue(withIdentifier: "start", sender: nil)
+                }
+            }
+                     
+               
+               }catch let err{
+                    print("Error", err)
+               }
+           }.resume()
+       }
+   func GenerateUUID() -> String{
+          let uuid = UUID().uuidString
+          defaults.set(uuid, forKey: "UUID")
+          return uuid
+      }
+    func memberID() -> String {
+          let uuid = defaults.string(forKey: "UUID")!
+          return uuid
+      }
+    func room_code() -> String {
+          return self.defaults.string(forKey: "room_code")!
+      }
+    
+    
+    struct CheckStatus: Codable {
+        var status: Bool?
+          let description: String?
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+      
+      private enum CodingKeys: String, CodingKey{
+          case status
+          case description
+       }
+      }
 
 }
